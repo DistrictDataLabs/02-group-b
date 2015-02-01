4aff4bc0474d6e385283e8a98d3f969f69154018
William Sankey <WSankey@gmail.com>
2015-02-01 18:17:20 -0500

    Added JSGlue.

--- static/script.js
+++ static/script.js
@@ -7,8 +7,8 @@ $(document).ready(function() {
   //Reading map file and data
 
   queue()
-  .defer(d3.json, {{url_for('static', filename='us.json')}})
-  .defer(d3.json, {{url_for('static', filename='dewmvpv1.json')}})
+  .defer(d3.json, Flask.url_for("static", {"filename": 'us.json'}))
+  .defer(d3.json, Flask.url_for("static", {"filename":'dewmvpv1.json'}))
   .await(ready);
 
   function ready(error, us, food_data) {
