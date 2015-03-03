7857521dea418d3613f50a13d031a829f9826bfa
William Sankey <WSankey@gmail.com>
2015-03-02 23:46:43 -0500

    Different layout.

--- app.py
+++ app.py
@@ -11,5 +11,17 @@ def home():
 def about():
     return render_template("about.html")
 
+#About page
+@app.route('/visualizations')
+def visualizations():
+    return render_template("visualizations.html")
+
+#About page
+@app.route('/data')
+def data():
+    return render_template("data.html")
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
